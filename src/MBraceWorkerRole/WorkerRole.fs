0aecc09bd14795400aa5cfa13b625999c23953f1
isaacabraham <isaac.abraham@gmail.com>
2015-02-28 19:29:53 +0000

    Initial code commit.

---
+++ src/MBraceWorkerRole/WorkerRole.fs
@@ -0,0 +1,42 @@
+﻿namespace MBraceWorkerRole
+
+open MBrace.Azure.Runtime
+open MBrace.Azure.Runtime.Common
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.ServiceRuntime
+open System
+open System.Diagnostics
+open System.Net
+
+type WorkerRole() =
+    inherit RoleEntryPoint() 
+
+    let getSetting = CloudConfigurationManager.GetSetting
+    let setEnv key value = Environment.SetEnvironmentVariable(key, value)
+
+    let svc =
+        lazy
+            let config =
+                Configuration
+                    .Default
+                    .WithStorageConnectionString(getSetting "StorageConnection")
+                    .WithServiceBusConnectionString(getSetting "ServiceBusConnection")
+            let svc =
+                Service(config,
+                    serviceId = RoleEnvironment.CurrentRoleInstance.Id,
+                    MaxConcurrentJobs = Environment.ProcessorCount)
+
+            svc.AttachLogger(CustomLogger(fun text -> Trace.WriteLine text))
+            svc
+
+    let log message (kind:string) = Trace.TraceInformation(message, kind)
+
+    override __.Run() = svc.Value.Start()
+    override __.OnStart() =
+        let customTempLocalResourcePath = RoleEnvironment.GetLocalResource("CustomTempLocalStore").RootPath
+        customTempLocalResourcePath |> setEnv "TMP"
+        customTempLocalResourcePath |> setEnv "TEMP"
+        ServicePointManager.DefaultConnectionLimit <- 512
+        svc.Force() |> ignore
+        base.OnStart()        
+    override __.OnStop() = base.OnStop()
